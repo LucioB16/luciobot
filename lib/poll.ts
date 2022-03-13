d1f4207acdb3f982bfe8298e31997314db63af10
LucioB16 <luciobotacin@gmail.com>
2022-03-12 21:41:43 -0300

    Add: Poll CRUD

--- lib/poll.ts
+++ lib/poll.ts
@@ -1,9 +1,11 @@
-import { Poll, PrismaClient } from '@prisma/client'
+import { Poll, Option, Vote, PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+export type PollWithOptions = Poll & {options: OptionWithVoters[]}
+export type OptionWithVoters = Option & {voters: Vote[]}
 
-export const getPoll = async (pollId: number) : Promise<Poll | null> => {
-  return await prisma.poll.findUnique({
+export const getPoll = async (pollId: number) : Promise<PollWithOptions | null> => {
+  const prisma = new PrismaClient()
+  let poll = await prisma.poll.findUnique({
     where: {
       id: pollId
     },
@@ -15,14 +17,81 @@ export const getPoll = async (pollId: number) : Promise<Poll | null> => {
       }
     }
   })
+  await prisma.$disconnect()
+  return poll
 }
 
-export const createPoll = async (poll: Poll) : Promise<Poll> => {
-  return await prisma.poll.create({
-    data: poll
+export const createPoll = async (poll: Poll) : Promise<PollWithOptions> => {
+  const prisma = new PrismaClient()
+  let newPoll = await prisma.poll.create({
+    data: poll,
+    include: {
+      options: {
+        include: {
+          voters: true
+        }
+      }
+    }
   })
+  await prisma.$disconnect()
+  return newPoll
 }
 
-// export const updatePoll = async (poll: Poll) : Promise<Poll> => {
-//   await prisma.poll.u
-// }
+export const updatePoll = async (poll: Poll) : Promise<Poll> => {
+  const prisma = new PrismaClient()
+  let updatedPoll = await prisma.poll.update({
+    where: {
+      id: poll.id
+    },
+    data: poll,
+    include: {
+      options: {
+        include: {
+          voters: true
+        }
+      }
+    }
+  })
+  await prisma.$disconnect()
+  return updatedPoll
+}
+
+export const deletePoll = async (pollId: number) : Promise<Poll> => {
+  const prisma = new PrismaClient()
+  let deletedPoll = await prisma.poll.delete({
+    where: {
+      id: pollId
+    },
+    include: {
+      options: {
+        include: {
+          voters: true
+        }
+      }
+    }
+  })
+  await prisma.$disconnect()
+  return deletedPoll
+}
+
+export const addOption = async (option: Option, pollId: number) : Promise<void> => {
+  const prisma = new PrismaClient()
+  await prisma.poll.update({
+    where: {
+      id: pollId
+    },
+    include: {
+      options: true
+    },
+    data: {
+      options: {
+        push:
+      }
+    }
+  })
+  let poll = await getPoll(pollId)
+
+  poll?.options.push(option)
+
+  return
+}
